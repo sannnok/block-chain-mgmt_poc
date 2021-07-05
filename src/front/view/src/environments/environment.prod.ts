8ee07b90c0f57a6837b2e882eae87f76536290ea
Alexandr <arudacov@vivatinc.com>
2021-07-05 11:28:59 +0300

    Cors fix

--- src/front/view/src/environments/environment.prod.ts
+++ src/front/view/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  sUrl: 'http://beingno.one:/',
+  sUrl: 'http://beingno.one:3000/parse/',
 };
--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,25 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
   });
+
+  // Case whitelist defined
+  const whitelist = ['http://beingno.one'];
+  app.enableCors({
+    origin: (origin, callback) => {
+      console.log("handling....", origin);  
+    if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
